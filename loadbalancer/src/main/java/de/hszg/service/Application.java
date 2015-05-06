b931d094e40cecc6ce861e1b85dec5b71ae0bd40
Daniel Müssig <danielmpla@gmail.com>
2015-05-06 09:17:25 +0200

    fixed project for jetty

--- loadbalancer/src/main/java/de/hszg/service/Application.java
+++ loadbalancer/src/main/java/de/hszg/service/Application.java
@@ -1,17 +1,10 @@
 package de.hszg.service;
 
-import org.glassfish.jersey.server.ResourceConfig;
-
 import javax.ws.rs.ApplicationPath;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Created by Andre on 05.05.2015.
  */
 @ApplicationPath("/")
-public class Application extends ResourceConfig {
-    public Application() {
-        packages("de.hszg.service");
-    }
+public class Application extends javax.ws.rs.core.Application {
 }
